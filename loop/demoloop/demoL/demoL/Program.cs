731ce85ac948fbb11c8a2528e230e10c0682f2dc
ezhil0512 <ezhilgobal@gmail.com>
2025-07-24 12:35:23 +0530

    commit  message

--- loop/demoloop/demoL/demoL/Program.cs
+++ loop/demoloop/demoL/demoL/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace loop
+namespace demoL
 {
-    class Class1
+    class Program
     {
-        public static void print()
+        static void Main(string[] args)
         {
-            Console.WriteLine("hello world..");
         }
     }
 }
