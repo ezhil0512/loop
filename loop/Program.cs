4b24e02aed2ba1eb490b863025e704217013878d
ezhil0512 <ezhilgobal@gmail.com>
2025-07-24 12:09:48 +0530

    commit message

---
+++ loop/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("enter the number");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter the condition");
+            //int c = int.Parse(Console.ReadLine());
+            //for (int i = n; i <= c; ++i)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+           // int i = 1;
+           // Console.WriteLine(i);
+           // Console.WriteLine(++i);
+            //int i = 1;
+            //while (i <= 10)
+            //{
+              //  Console.WriteLine(i);
+                //i++;
+            //}
+
+
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("ezhil");
+            //}
+
+            //int i=1;
+            //while (i <= 5)
+              
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
